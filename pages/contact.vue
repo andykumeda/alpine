fbc1461204e831dcc08ca0078f2b332cdacc6fe5
Andy Kumeda <andy@delta9.tech>
2023-05-10 18:04:18 -0700

    changed to formspree to process

--- pages/contact.vue
+++ pages/contact.vue
@@ -23,7 +23,7 @@
 
     <p class="my-4 text=xl font-bold">Send me a message!</p>
 
-    <form class="bg-white p-6 rounded-lg shadow-md" accept-charset="UTF-8" action="https://www.formbackend.com/f/5c18ace58eef8648" method="POST">
+    <form class="bg-white p-6 rounded-lg shadow-md" accept-charset="UTF-8" action="https://formspree.io/f/mzbqwbql" method="POST">
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2" for="name">Name</label>
         <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="name" name="name" required>
