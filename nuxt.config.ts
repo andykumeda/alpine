d9a6c20f5ba023891013bbf054ced6b797c02a3f
Andy Kumeda <andy@delta9.tech>
2023-05-10 18:05:47 -0700

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 export default defineNuxtConfig({
+    app: {
+        head: {
+            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }]
+        }
+    },
+
   extends: '@nuxt-themes/alpine',
   css: [
-    '@/assets/css/main.css',
+    '@/assets/css/main.css'
   ],
     postcss: {
         plugins: {
