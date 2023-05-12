5e929d7eef10b68e7e7550e8d979a25042693578
Andy Kumeda <andy@delta9.tech>
2023-05-12 12:36:38 -0700

    added border colors

--- pages/contact.vue
+++ pages/contact.vue
@@ -20,26 +20,25 @@
     <a href="sms:3104931793" class="ml-2 text-blue-500 hover:text-blue-700">Chat</a>
   </div>
 </div>
-
     <p class="my-4 text=xl font-bold">Send me a message!</p>
 
-    <form class="bg-white p-6 rounded-lg shadow-md" accept-charset="UTF-8" action="https://formspree.io/f/mzbqwbql" method="POST">
+    <form class="bg-white outline p-6 rounded-lg shadow-md outline-gray-500" accept-charset="UTF-8" action="https://formspree.io/f/mzbqwbql" method="POST">
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2" for="name">Name</label>
-        <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" id="name" name="name" required>
+        <input class="input input-bordered input-primary w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="text" id="name" name="name" required>
       </div>
 
       <div class="mb-4">
         <label class="block text-gray-700 font-bold mb-2" for="email">Email</label>
-        <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" id="email" name="email" required>
+        <input class="input input-bordered input-primary w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="email" id="email" name="email" required>
       </div>
 
       <div class="mb-6">
         <label class="block text-gray-700 font-bold mb-2" for="message">Message</label>
-        <textarea class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="message" id="message" rows="5" cols="25" required></textarea>
+        <textarea class="textarea textarea-primary w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" name="message" id="message" rows="5" cols="25" required></textarea>
       </div>
 
-      <button class="bg-red-500 hover:bg-red-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border border-red-700" type="submit">Submit</button>
+      <button class="btn btn-active btn-secondary" type="submit">Submit</button>
 
     </form>
   </div>
