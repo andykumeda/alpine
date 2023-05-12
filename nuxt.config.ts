174e27dfd6b40e6a5f03e7018f3f8ea27880278d
Andy Kumeda <andy@delta9.tech>
2023-05-12 09:07:07 -0700

    added @nuxtjs/device for my hack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,18 @@
 import path from 'path'
 
 export default defineNuxtConfig({
-    app: {
-        head: {
-            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }]
-        }
-    },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }]
+    }
+  },
+  
+  extends: 'themes/alpine',
 
-  extends: 'themes/alpine-theme',
   themes: [
     {
-      name: 'alpine-theme', // the name of your theme
-      src: path.resolve(__dirname, 'themes/alpine-theme'), // the path to your theme directory
+      name: 'alpine', // the name of your theme
+      src: path.resolve(__dirname, 'themes/alpine'), // the path to your theme directory
       // any other options you want to pass to the theme module
     }
   ],
@@ -19,11 +20,17 @@ export default defineNuxtConfig({
   css: [
     'assets/css/main.css'
   ],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
+  },
+
+  modules: [
+    '@nuxtjs/device',
+  ]
+
 });
 
