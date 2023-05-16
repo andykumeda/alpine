b44f943b8526185ae1c952fb33be07b58f7b423e
Andy Kumeda <andy@delta9.tech>
2023-05-16 10:32:32 -0700

    changed link appearance

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,21 +5,22 @@
 <div style="margin-left: 20px;">
   <div class="flex items-center">
     <Icon name="mdi:phone-classic" />
-    <a href="tel:3104931793" class="ml-2 text-blue-500 hover:text-blue-700">Phone</a>
+    <a href="tel:3104931793" class="ml-2 text-white hover:underline hover:text-blue-300">Phone</a>
   </div>
   <div class="flex items-center mt-2">
     <Icon name="mdi:email" />
-    <a href="mailto:0xandyk@gmail.com" class="ml-2 text-blue-500 hover:text-blue-700">Email</a>
+    <a href="mailto:0xandyk@gmail.com" class="ml-2 text-white hover:underline hover:text-blue-300">Email</a>
   </div>
   <div class="flex items-center mt-2">
     <Icon name="mdi:telegram" />
-    <a href="https://t.me/andyk3406" class="ml-2 text-blue-500 hover:text-blue-700">Telegram</a>
+    <a href="https://t.me/andyk3406" class="ml-2 text-white hover:underline hover:text-blue-300">Telegram</a>
   </div>
   <div class="flex items-center mt-2">
     <Icon name="mdi:chat" />
-    <a href="sms:3104931793" class="ml-2 text-blue-500 hover:text-blue-700">Chat</a>
+    <a href="sms:3104931793" class="ml-2 text-white hover:underline hover:text-blue-300">Chat</a>
   </div>
 </div>
+
     <p class="my-4 text=xl font-bold">Send me a message!</p>
 
     <form class="bg-gray-200 outline p-6 rounded-lg shadow-md outline-gray-500" accept-charset="UTF-8" action="https://formspree.io/f/mzbqwbql" method="POST">
@@ -38,7 +39,7 @@
         <textarea class="textarea textarea-primary w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" name="message" id="message" rows="5" cols="25" required></textarea>
       </div>
 
-      <button class="btn btn-active btn-secondary" type="submit">Submit</button>
+      <button class="btn btn-active btn-primary" type="submit">Submit</button>
 
     </form>
   </div>
