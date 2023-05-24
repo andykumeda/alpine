1da6281375a66e4451e9cb8a59b7e624be2d6788
Andy Kumeda <andy@delta9.tech>
2023-05-23 17:22:48 -0700

    added back to top functionality. Need to clean up later.

--- themes/alpine/components/AppFooter.vue
+++ themes/alpine/components/AppFooter.vue
@@ -27,6 +27,7 @@ function handleScroll() {
   scrollPosition.value = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 }
 
+
 </script>
 
 <template>
@@ -56,8 +57,8 @@ function handleScroll() {
       </div>
     </div>
       <button v-if="scrollPosition >= linesThreshold" id="to-top-button" @click="goToTop" class="back-to-top-button" title="Go To Top">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-          <path d="M12 4l8 8h-6v8h-4v-8H4l8-8z" />
+        <svg class="svg-icon" viewBox="0 0 20 20">
+          <path d="M13.889,11.611c-0.17,0.17-0.443,0.17-0.612,0l-3.189-3.187l-3.363,3.36c-0.171,0.171-0.441,0.171-0.612,0c-0.172-0.169-0.172-0.443,0-0.611l3.667-3.669c0.17-0.17,0.445-0.172,0.614,0l3.496,3.493C14.058,11.167,14.061,11.443,13.889,11.611 M18.25,10c0,4.558-3.693,8.25-8.25,8.25c-4.557,0-8.25-3.692-8.25-8.25c0-4.557,3.693-8.25,8.25-8.25C14.557,1.75,18.25,5.443,18.25,10 M17.383,10c0-4.07-3.312-7.382-7.383-7.382S2.618,5.93,2.618,10S5.93,17.381,10,17.381S17.383,14.07,17.383,10"></path>
         </svg>
         <span class="sr-only">Go to top</span>
       </button>
@@ -67,6 +68,18 @@ function handleScroll() {
 
 <style scoped lang="ts">
 css({
+  '.svg-icon': {
+    width: '3em',
+    height: '3em',
+   },
+  '.svg-icon path, .svg-icon polygon, .svg-icon rect': {
+   fill: '#4691f6',
+  },
+
+  '.svg-icon circle': {
+    stroke: '#4691f6',
+  },
+
   '.back-to-top-button': {
     position: 'fixed',
     bottom: '2rem',
@@ -85,6 +98,7 @@ css({
     boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
     transition: 'background-color 0.3s ease-in-out',
   },
+
 
   footer: {
     display: 'flex',
